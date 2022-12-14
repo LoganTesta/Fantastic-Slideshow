2fea98fa26fb84422c957f48e1d4b5f64d2d8041
logan <logan.testa@outlook.com>
2022-12-13 22:40:25 -0800

    Update slideshow logic

--- assets/javascript/slideshow.js
+++ assets/javascript/slideshow.js
@@ -1,33 +1,43 @@
 
 window.addEventListener("load", function() {
-
-    /*Start of Slideshow */
+    
+    /* Start of Slideshow */
     let currentSlide;
+    let prevSlide;
     let slideshowCounter = 0;
     let paused = false;
     let updateSlideSettings = true;
+    let regularSwitchSlide = false;
+    let switchText = false;
     let currentSlideNumber = 0;
     const maxSlideNumber = 2;
     let pausePlayButton;
     let slideTitle;
+    let slide0 = new Image(670, 400);
+    slide0.src = "";
+    let slide1 = new Image(670, 400);
+    slide1.src = "";
+    let slide2 = new Image(670, 400);
+    slide2.src = "";
 
     let slideButton0 = document.getElementById('slideButton0');
     let slideButton1 = document.getElementById('slideButton1');
     let slideButton2 = document.getElementById('slideButton2');
 
     slideButton0.addEventListener('click', function () {
-        setSlide(0);
+       setSlide(0);
     }, false);
     slideButton1.addEventListener('click', function () {
-        setSlide(1);
+       setSlide(1);
     }, false);
     slideButton2.addEventListener('click', function () {
-        setSlide(2);
+       setSlide(2);
     }, false);
-
 
     let backIcon = document.getElementsByClassName("slideshow__icon")[0];
     let forwardIcon = document.getElementsByClassName("slideshow__icon")[1];
+
+    let currentSlideImageLink = document.getElementsByClassName("slide__link")[0];
 
     backIcon.addEventListener('click', function () {
        setSlide(currentSlideNumber - 1);
@@ -40,40 +50,43 @@ window.addEventListener("load", function() {
 
 
     function init() {
-        currentSlide = document.getElementsByClassName("slide__image")[0];
+        prevSlide = document.getElementsByClassName("slide__image")[0];
+        currentSlide = document.getElementsByClassName("slide__image")[1];
         slideTitle = document.getElementsByClassName("slide__title")[0];
         pausePlayButton = document.getElementById("pausePlayButton");
         currentSlide.style.opacity = 0;
+        prevSlide.style.opacity = 0;
+        currentSlideImageLink.href = "";
+        currentSlideImageLink.setAttribute("aria-label", "Slide 0");
+        currentSlideImageLink.innerHTML = "";
         setInterval(function () {
             runFunctions();
         }, 10);
     }
     window.onload = init();
 
-
     function runFunctions() {
         runSlideShow();
     }
 
     function runSlideShow() {
-
         if (paused === false) {
-            if (slideshowCounter === 0) {
-                currentSlide.style.opacity = 0;
+            if (slideshowCounter === 200) {
+                switchText = true;
             }
-            if (slideshowCounter < 200) {
-                currentSlide.style.opacity = parseFloat(currentSlide.style.opacity) + 0.005;
+            if (slideshowCounter < 400) {
+                currentSlide.style.opacity = parseFloat(currentSlide.style.opacity) + 0.0025;
+                prevSlide.style.opacity = parseFloat(prevSlide.style.opacity) - 0.0025;
             }
-            if (200 <= slideshowCounter && slideshowCounter < 700) {
+            if (400 <= slideshowCounter && slideshowCounter < 900) {
                 currentSlide.style.opacity = 1;
+                prevSlide.style.opacity = 0;
             }
-            if (slideshowCounter >= 700) {
-                currentSlide.style.opacity = parseFloat(currentSlide.style.opacity) - 0.005;
-            }
+
             if (slideshowCounter >= 900) {
                 slideshowCounter = 0;
                 updateSlideSettings = true;
-                currentSlide.style.opacity = 0;
+                regularSwitchSlide = true;
                 currentSlideNumber++;
             }
 
@@ -85,21 +98,49 @@ window.addEventListener("load", function() {
 
             if (updateSlideSettings) {
                 updateSlideSettings = false;
+                if(regularSwitchSlide){
+                    regularSwitchSlide = false;
+                    currentSlide.style.opacity = 0;
+                    prevSlide.style.opacity = 1;
+                }
+
                 if (currentSlideNumber === 0) {
-                    slideTitle.innerHTML = "Slide 1";
-                    slideButton0.style.opacity = 1.0;
-                    slideButton1.style.opacity = 0.40;
-                    slideButton2.style.opacity = 0.40;
+                    slideTitle.innerHTML = "Slide 0";
+                    currentSlide.style.backgroundImage = "url(" + slide0.src + ")";
+                    prevSlide.style.backgroundImage = "url(" + slide2.src + ")";
+                    slideButton0.classList.add('currentSlideButton');
+                    slideButton1.classList.remove('currentSlideButton');
+                    slideButton2.classList.remove('currentSlideButton');                 
                 } else if (currentSlideNumber === 1) {
-                    slideTitle.innerHTML = "Slide 2";
-                    slideButton0.style.opacity = 0.40;
-                    slideButton1.style.opacity = 1.0;
-                    slideButton2.style.opacity = 0.40;
+                    slideTitle.innerHTML = "Slide 1";
+                    currentSlide.style.backgroundImage = "url(" + slide1.src + ")";
+                    prevSlide.style.backgroundImage = "url(" + slide0.src + ")";
+                    slideButton0.classList.remove('currentSlideButton');
+                    slideButton1.classList.add('currentSlideButton');
+                    slideButton2.classList.remove('currentSlideButton');               
                 } else if (currentSlideNumber === 2) {
-                    slideTitle.innerHTML = "Slide 3";
-                    slideButton0.style.opacity = 0.40;
-                    slideButton1.style.opacity = 0.40;
-                    slideButton2.style.opacity = 1.0;
+                    slideTitle.innerHTML = "Slide 2";
+                    currentSlide.style.backgroundImage = "url(" + slide2.src + ")";
+                    prevSlide.style.backgroundImage = "url(" + slide1.src + ")";
+                    slideButton0.classList.remove('currentSlideButton');
+                    slideButton1.classList.remove('currentSlideButton');
+                    slideButton2.classList.add('currentSlideButton');                 
+                } 
+            }
+            if (switchText){
+                switchText = false;
+                if (currentSlideNumber === 0) {
+                    currentSlideImageLink.href = "";
+                    currentSlideImageLink.setAttribute("aria-label", "Slide 0");
+                    currentSlideImageLink.innerHTML = "";
+                } else if (currentSlideNumber === 1) {
+                    currentSlideImageLink.href = "";
+                    currentSlideImageLink.setAttribute("aria-label", "Slide 1");
+                    currentSlideImageLink.innerHTML = "";
+                } else if (currentSlideNumber === 2) {
+                    currentSlideImageLink.href = "";
+                    currentSlideImageLink.setAttribute("aria-label", "Slide 2");
+                    currentSlideImageLink.innerHTML = "";
                 } 
             }
             slideshowCounter++;
@@ -114,19 +155,22 @@ window.addEventListener("load", function() {
             pausePlayButton.classList.add("paused");
         }
     }
+
     let pausePlay = document.getElementById("pausePlayButton");
     pausePlay.addEventListener("click", togglePausePlay, false);
 
     function setSlide(slideNumber) {
-        slideshowCounter = 50;
+        slideshowCounter = 400;
         currentSlideNumber = slideNumber;
         paused = false;
         pausePlayButton.classList.remove("paused");
         updateSlideSettings = true;
+        switchText = true;
     }
-
-
-   // Allow touch events to interact with slideshow.
+    
+    
+    
+    // Allow touch events to interact with slideshow.
     let initialTouchX = 0;
 
     let slideshowImage = document.getElementsByClassName("slide")[0];
@@ -174,5 +218,6 @@ window.addEventListener("load", function() {
             setSlide(currentSlideNumber + 1);
         }            
     }
-
+    
+    
 }, "false");
