85eeabc696d4e2be7d43cb8b1b00870209187afd
logan <logan.testa@outlook.com>
2022-12-22 21:02:04 -0800

    Let slideshow have variable number of slides

--- assets/javascript/slideshow.js
+++ assets/javascript/slideshow.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function() {
     let regularSwitchSlide = false;
     let switchText = false;
     let currentSlideNumber = 0;
-    const maxSlideNumber = 3;
+    const maxSlideNumber = document.getElementsByClassName("slide").length - 1;
     let pausePlayButton;
     let slideTitle;
  
