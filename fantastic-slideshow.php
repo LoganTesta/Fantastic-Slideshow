4dbeda5a2c69312fa7b91017f9fdda3e92dd229c
logan <logan.testa@outlook.com>
2023-02-18 20:55:05 -0800

    Let user show or hide arrows

--- fantastic-slideshow.php
+++ fantastic-slideshow.php
@@ -67,7 +67,8 @@ function fs_register_settings() {
     add_option( 'fantastic-slideshow-minimum-mouse-drag-distance', "100" );
     add_option( 'fantastic-slideshow-enable-touch-dragging', 'yes' );
     add_option( 'fantastic-slideshow-enable-mouse-dragging', 'yes' );
-    
+    add_option( 'fantastic-slideshow-show-arrows', 'yes' );
+   
     register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-leading-text', 'fs_validatetextfield' );
     register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-leading-text-position', 'fs_validatetextfield' );
     register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-image-width', 'fs_validatetextfield' );
@@ -80,6 +81,7 @@ function fs_register_settings() {
     register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-minimum-mouse-drag-distance', 'fs_validatetextfield' );
     register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-enable-touch-dragging', 'fs_validatetextfield' );
     register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-enable-mouse-dragging', 'fs_validatetextfield' );
+    register_setting( 'fantastic-slideshow-settings-group', 'fantastic-slideshow-show-arrows', 'fs_validatetextfield' );
 }
 add_action( 'admin_init', 'fs_register_settings' );
 
@@ -172,6 +174,14 @@ function fs_generate_settings_page() {
                 <label class="admin-input-container__label--right" for="fantasticSlideshowEnableMouseDragging1">Yes</label>
                 <input id="fantasticSlideshowEnableMouseDragging1" class="admin-input-container__input fantastic-slideshow-enable-mouse-dragging" name="fantastic-slideshow-enable-mouse-dragging" type="radio" value="yes" <?php if( get_option( 'fantastic-slideshow-enable-mouse-dragging' ) === "yes" ) { echo "checked='checked'"; }; ?> />
                 <span class="admin-input-container__default-settings-text">Default: Yes. Mouse dragging enabled</span>
+            </div>
+                    <div class="admin-input-container">
+                <span class="admin-input-container__label">Show Arrows</span>
+                <label class="admin-input-container__label--right" for="fantasticSlideshowShowArrows0">No</label>
+                <input id="fantasticSlideshowShowArrows0" class="admin-input-container__input fantastic-slideshow-show-arrows" name="fantastic-slideshow-show-arrows" type="radio" value="hide" <?php if( get_option( 'fantastic-slideshow-show-arrows' ) === "hide" ) { echo "checked='checked'"; }; ?> />
+                <label class="admin-input-container__label--right" for="fantasticSlideshowShowArrows1">Yes</label>
+                <input id="fantasticSlideshowShowArrows1" class="admin-input-container__input fantastic-slideshow-show-arrows" name="fantastic-slideshow-show-arrows" type="radio" value="show" <?php if( get_option( 'fantastic-slideshow-show-arrows' ) === "show" ) { echo "checked='checked'"; }; ?> />
+                <span class="admin-input-container__default-settings-text">Default: Yes, show arrows</span>
             </div>
             <?php submit_button(); ?>
         </form>
@@ -465,10 +475,10 @@ function fs_load_slideshows( $a ) {
         $pluginContainer .= '</div>';
         $count++;
     }
-    $pluginContainer .= '<div class="slideshow__icon left">';
+    $pluginContainer .= '<div class="slideshow__icon left ' . get_option( "fantastic-slideshow-show-arrows" ) . '">';
     $pluginContainer .= '<div class="slideshow__icon__link">&#10094;</div>';
     $pluginContainer .= '</div>';
-    $pluginContainer .= '<div class="slideshow__icon right">';
+    $pluginContainer .= '<div class="slideshow__icon right ' . get_option( "fantastic-slideshow-show-arrows" ) . '">';
     $pluginContainer .= '<div class="slideshow__icon__link">&#10095;</div>';
     $pluginContainer .= '</div>';
     $pluginContainer .= '</div>';
